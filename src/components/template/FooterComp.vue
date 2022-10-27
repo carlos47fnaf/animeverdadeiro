4ab00841dfdcc5437e265b395aa602b9119a86f8
carlos47fnaf <kaduardomello@gmail.com>
2022-10-27 09:24:20 -0300

    atu

---
+++ src/components/template/FooterComp.vue
@@ -0,0 +1,37 @@
+<style></style>
+<template>
+  <footer>
+    <div class="h"><h4>©Copyright 2022 by AnimesVue. All rights reversed.</h4></div>
+  </footer>
+</template>
+<style scoped>
+footer{
+ text-align: center;
+ align-items: center;
+height: 90px;
+background: #18171D;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+display: flex;
+  align-content: center;
+  justify-content: center;
+}
+
+  h4{
+  width: 285px;
+  height: 30px;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 15px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: white;
+  }
+  .h{
+    align-items: center;
+  }
+  
+  </style>
+  
---
+++ src/components/template/HeaderComp.vue
@@ -0,0 +1,42 @@
+<style></style>
+<template>
+  <header>
+    <div class="esquerdo">Animes<strong>Vue</strong></div>
+   
+    <div class="direita">Perfil</div>
+  </header>
+</template>
+<style scoped>
+header {
+  justify-content: space-between;
+  display: flex;
+  align-items: center;
+  height: 90px;
+  background: #18171d;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+a {
+  text-decoration: none;
+  color: white;
+}
+.esquerdo{
+  margin-left: 20px;
+  font-size: 30px;
+}
+
+strong{
+  color: #ff0b53;
+}
+.direita{
+  margin-right: 20px;
+}
+
+input {
+  width: 300px;
+  height: 30px;
+  display: flex;
+  align-content: center;
+  
+}
+
+</style>
